3d4e0b0833a3a47c305ffd810303ef63777d8e10
Deepanshu Punia <puniadeepanshu21@gmail.com>
2026-02-23 13:04:54 -0500

    Initial Mini SIEM Dashboard

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, jsonify
+from flask import request, redirect, url_for
+import random
+
+app = Flask(__name__)
+
+logs = []
+
+@app.route("/")
+def dashboard():
+
+    parsed_alerts = []
+
+    for log in logs:
+        severity = "LOW"
+
+        if "FAILED" in log.upper() or "DENIED" in log.upper():
+            severity = "HIGH"
+        elif "ERROR" in log.upper():
+            severity = "MEDIUM"
+
+        parsed_alerts.append({
+            "ip": "dynamic",
+            "event": log,
+            "severity": severity
+        })
+
+    return render_template("dashboard.html", alerts=parsed_alerts)
+
+@app.route("/help")
+def help_page():
+    return render_template("help.html")
+
+@app.route("/generate-demo")
+def generate_demo():
+    sample_events = [
+        "Failed login from 192.168.1.5",
+        "Accepted login for admin",
+        "ERROR invalid request",
+        "DENIED connection attempt",
+        "Successful login"
+    ]
+    
+
+    for i in range(15):
+        logs.append(random.choice(sample_events))
+
+    return jsonify({"status":"demo logs added"})
+
+@app.route("/upload", methods=["POST"])
+def upload_logs():
+    file = request.files.get("logfile")
+
+    if not file:
+        return redirect(url_for("dashboard"))
+
+    content = file.read().decode("utf-8", errors="ignore")
+
+    for line in content.splitlines():
+        if line.strip():
+            logs.append(line)
+
+    return redirect(url_for("dashboard"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
